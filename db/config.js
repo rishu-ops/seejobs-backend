b5f25abfa8ad9bd29ee1b7be5df582e4a015fa41
rishu <rock.jade09@gmail.com>
2024-07-27 17:32:35 +0530

    updated code

--- db/config.js
+++ db/config.js
@@ -1,18 +1,15 @@
-
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://rishu:rishu@cluster0.sovnymf.mongodb.net/');
-    console.log('MongoDB connected');
+    await mongoose.connect(
+      "mongodb+srv://shashikant:seejob123@cluster0.elsxfve.mongodb.net/"
+    );
+    console.log("MongoDB connected");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
-  
   }
-
 };
 
 export default connectDB;
-
-
